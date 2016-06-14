1debe989f0ff9b1372fdc3f06f8f337476d0d601
Chris Anthony <CTAnthny@gmail.com>
2016-06-14 18:16:29 -0400

    First commit

---
+++ lib/cell.rb
@@ -0,0 +1,23 @@
+# A class that represents an individual cell in the minefield.
+class Cell
+  def initialize
+    @mine = false
+    @revealed = false
+  end
+
+  def place_mine
+    @mine = true
+  end
+
+  def contains_mine?
+    @mine
+  end
+
+  def reveal!
+    @revealed = true
+  end
+
+  def revealed?
+    @revealed
+  end
+end
---
+++ lib/minefield.rb
@@ -0,0 +1,75 @@
+require_relative "cell"
+
+class Minefield
+  attr_reader :row_count, :column_count
+
+  def initialize(row_count, column_count, mine_count)
+    @column_count = column_count
+    @row_count = row_count
+    @mine_count = mine_count
+
+    build_minefield
+    plant_mines
+  end
+
+  # Return true if the cell been uncovered, false otherwise.
+  def cell_cleared?(row, col)
+    false
+  end
+
+  # Uncover the given cell. If there are no adjacent mines to this cell
+  # it should also clear any adjacent cells as well. This is the action
+  # when the player clicks on the cell.
+  def clear(row, col)
+  end
+
+  # Check if any cells have been uncovered that also contained a mine. This is
+  # the condition used to see if the player has lost the game.
+  def any_mines_detonated?
+    false
+  end
+
+  # Check if all cells that don't have mines have been uncovered. This is the
+  # condition used to see if the player has won the game.
+  def all_cells_cleared?
+    false
+  end
+
+  # Returns the number of mines that are surrounding this cell (maximum of 8).
+  def adjacent_mines(row, col)
+    0
+  end
+
+  # Returns true if the given cell contains a mine, false otherwise.
+  def contains_mine?(row, col)
+    false
+  end
+
+  private
+
+  # Create a data structure within an instance variable that will represent
+  # the minefield.
+  def build_minefield
+    @minefield = []
+    20.times do
+      row = []
+      20.times do
+        row << Cell.new
+      end
+      @minefield << row
+    end
+  end
+
+  # Distribute 65 mines amongst the individual minefield cells
+  def plant_mines
+    mine_counter = 65
+    while mine_counter > 0
+      rand_num1 = rand(20) + 1
+      rand_num2 = rand(20) + 1
+      if !@minefield[rand_num1][rand_num2].contains_mine?
+        @minefield[rand_num1][rand_num2].place_mine
+        mine_counter -= 1
+      end
+    end
+  end
+end
